bcd9a5bee7e234fd05ef7eab0cc5f8fb185f4a6e
Khushpreet Kaur <156661182+khush-preet-2004@users.noreply.github.com>
2025-08-17 20:07:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./db/connectDb");
+const app = express();
+const PORT = 4000;
+require("dotenv").config();
+const path = require("path");
+
+const todoRouter = require("./routes/todo.routes")
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,"public")));
+
+app.use("/todo",todoRouter);
+
+app.get("/", (req, res) => {	});
+
+connectDB().then(()=>{
+  app.listen(PORT, () => console.log("Server running on port " + PORT));
+}).catch((error)=>console.log(error))
